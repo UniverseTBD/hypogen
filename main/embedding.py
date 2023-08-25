82d030470a5307a43ad112f4f5cb3a4cb59ecfcd
charlieoneill11 <cponeill00@gmail.com>
2023-08-25 10:21:19 +1000

    fixed embeddings

--- main/embedding.py
+++ main/embedding.py
@@ -1,3 +1,21 @@
+import csv
+import json
+from pathlib import Path
+
+# Load the JSON data
+file_path = '../data/processed/extracted_preprocessed.json'
+json_data = json.loads(Path(file_path).read_text())
+
+# Write to a CSV file
+csv_file_path = '../data/processed/extracted_preprocessed.csv'
+with open(csv_file_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    # Write header
+    csvwriter.writerow(['text'])
+    # Write data
+    for entry in json_data:
+        csvwriter.writerow([entry['text']])
+
 import os
 import yaml
 import json
@@ -5,6 +23,7 @@ from pathlib import Path
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.docstore.document import Document
+from langchain.document_loaders.csv_loader import CSVLoader
 
 # Load configuration and set API key
 config = yaml.safe_load(open("../config.yml"))
@@ -20,13 +39,13 @@ embeddings = OpenAIEmbeddings(
     model="text-embedding-ada-002",
     openai_api_key=API_KEY,
     openai_api_type="azure",
+    chunk_size=16,
 )
 
 # Load the JSON file and extract the 'text' attribute for each dictionary
-file_path = '../data/processed/extracted_preprocessed.json'
-json_data = json.loads(Path(file_path).read_text())
-documents = [Document(page_content=entry['text'], metadata={"source": "local"}) for entry in json_data]
-print(documents)
+csv_file_path = '../data/processed/extracted_preprocessed.csv'
+loader = CSVLoader(file_path=csv_file_path, source_column="text")
+documents = loader.load()
 
 # Define the persistence directory for Chroma
 persist_directory = "chroma_db"
@@ -40,7 +59,7 @@ vectordb = Chroma.from_documents(
 vectordb.persist()
 
 # Load from disk and query it
-query = "Black hole"
+query = "radial heating angular momentum"
 db3 = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
 docs = db3.similarity_search(query)
-print(docs[0].page_content)
+print(docs)
